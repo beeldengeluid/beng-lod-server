4afab363eca1f6078408cb78bc13c9ffe6ec4682
Willem Melder <wmelder@beeldengeluid.nl>
2019-05-17 15:28:00 +0200

    changed some comments

--- src/apis/lod/LODHandlerConcept.py
+++ src/apis/lod/LODHandlerConcept.py
@@ -1,29 +1,26 @@
 from rdflib import Graph
 from util.APIUtil import APIUtil
 
-#TODO this class is now just a copy of the LODHandler, implement it
 class LODHandlerConcept(object):
-	''' OpenSKOS platform provides RDF data for each concept. As far as I know the 
-		subdomain data.beeldengeluid.nl is forwarded to openskos platform on DNS level. 
+	''' OpenSKOS platform provides RDF data for each SKOS concept. As far as I know the 
+		sub domain data.beeldengeluid.nl is forwarded to OpenSKOS platform on DNS level. 
 		
-		For example the current concept resolving forwards to the Openskos that manages the paths
-		after the domain name:
+		Paths after the domain name are forwarded to OpenSKOS:
 			http://data.beeldengeluid.nl/<set>/<notation>, where <set>/<notation> is to be filled 
 			with, for example: 'gtaa' and '58027'.
 			
-		and by adding an extension the RDF/XML is also available:
+		... and by adding an '.rdf' extension the RDF/XML is also available:
 			http://data.beeldengeluid.nl/gtaa/58027.rdf
 			
-		This class makes the concept dereferencable on the rdlabs subdomain with <set> and <notation>
+		Note: this is not really a proper LOD practice. To fix that this class makes the concept 
+		dereferenceable on the data.rdlabs.beeldengeluid.nl sub domain with <set> and <notation>
 		paths:
 			http://data.rdlabs.beeldengeluid.nl/gtaa/58027
-			e.g.: `curl -X GET "http://localhost:5309/concept/gtaa/27948" -H "accept: application/json"`
-			
-		In the future the data.beeldengeluid.nl domain will be under control of the LOD server. For
-		that, a properly managed transition needs to be undertaken. 
+		e.g.: `curl -X GET "http://localhost:5309/concept/gtaa/27948" -H "accept: application/json"`
 		
 		The LOD server enables content negotiation, making it possible to get the data serialization 
-		in any format that RDFlib can handle, e.g. RDF/XML, Turtle, N3, JSON-LD and even.
+		in any format that RDFlib can handle, e.g. RDF/XML, Turtle, N3, JSON-LD. 
+		HTML is not yet supported.
 	'''
 	def __init__(self, config):
 		self.config = config
@@ -31,7 +28,6 @@ class LODHandlerConcept(object):
 	def getConceptRDF(self,set,notation,returnFormat):
 		graph = Graph()
 		uri = u'http://data.beeldengeluid.nl/%s/%s.rdf'%(set,notation)
-		print 'Debug: ', uri
 		graph.load( uri)
 		data = graph.serialize(format=returnFormat)
 		if data:
