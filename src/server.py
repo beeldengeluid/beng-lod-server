b2b5f8400d21edbdfd724e040305f66092ffbbb2
Eduardo Silva Navarrete <enavarrete@beeldengeluid.nl>
2019-08-27 11:45:10 +0200

    #31 HTML representation for BenG Schema

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request, Response
 from flask_cors import CORS
+import json
 
 from apis import api
 
@@ -22,6 +23,18 @@ api.init_app(
     description='LOD API mostly for e.g. dereferencing B&G resources')
 
 """------------------------------------------------------------------------------
+CUSTOM TEMPLATE FUNCTIONS
+------------------------------------------------------------------------------"""
+# Formats dates to be used in templates
+@app.template_filter()
+def datetimeformat(value, format='%d, %b %Y'):
+    return datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ").strftime(format)
+@app.template_filter()
+def getLastStringAfter(value):
+    lastIndex = value.rindex("/")
+    return value[lastIndex+1:len(value)]
+
+"""------------------------------------------------------------------------------
 PING / HEARTBEAT ENDPOINT
 ------------------------------------------------------------------------------"""
 
@@ -36,6 +49,42 @@ REGULAR ROUTING (STATIC CONTENT)
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+@app.route('/html-schema', methods=['GET'])
+@app.route('/html-schema/<string:language>/', defaults={'className': 'NONE'})
+@app.route('/html-schema/<string:language>/<string:className>')
+def htmlSchema(language=None, className=None):
+    CLASS_ROOT = "http://data.rdlabs.beeldengeluid.nl/schema"
+    DOMAIN = "http://www.w3.org/2000/01/rdf-schema#domain"
+    RANGE = "http://www.w3.org/2000/01/rdf-schema#range"
+    bengClasses = []
+    bengProps = []
+
+    with open('../resource/bengSchema.json', 'r') as bengSchema:
+        data = bengSchema.read()
+    obj = json.loads(data)
+
+    # parse class properties to feed table
+    if className != 'NONE':
+        classProps = []
+        for d in obj:
+            for k, v in d.items():
+                if k and k == '@type' and d[k][0].endswith('Property') and DOMAIN in d:
+                   dom = d[DOMAIN][0]['@id'] == CLASS_ROOT + "/" + className
+                   if d[DOMAIN][0]['@id'] == CLASS_ROOT + "/" + className:
+                       classProps.append(d)
+        print(len(classProps))
+        if len(classProps) == 0:
+            print("no own props!")
+        return render_template('schema.html', language=language, className=className, classProps=classProps)
+    for d in obj:
+        # parsing Schema (in json format)
+        for k, v in d.items():
+            if k and k == '@type' and d[k][0].endswith('Class'):
+                bengClasses.append(d)
+            elif k and k == '@type' and d[k][0].endswith('Property'):
+                bengProps.append(d)
+    return render_template('schema.html', language=language, bengClasses=bengClasses, bengProps=bengProps)
 
 if __name__ == '__main__':
 	app.run(host=app.config['APP_HOST'], port=app.config['APP_PORT'])
