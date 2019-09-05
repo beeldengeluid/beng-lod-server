28228e87bdbcd556773eb413fcc5db1564d13465
Jaap Blom <jblom@beeldengeluid.nl>
2019-09-05 15:28:32 +0200

    fixed the first concept handler test; adapted to concept handler to catch exceptions, so it can be properly tested

--- src/tests/unit_tests/apis/lod/lod_concept_handler_test.py
+++ src/tests/unit_tests/apis/lod/lod_concept_handler_test.py
@@ -6,29 +6,35 @@ from rdflib.plugin import PluginException
 
 """ ------------------------ fetchDocument -----------------------"""
 
-DUMMY_SET = "blabla"
-DUMMY_NOTATION = "123456"
-RETURN_TYPE = "JSON"
+DUMMY_SET = "dummy-set"
+DUMMY_NOTATION = "dummy-notation"
 
 # @pytest.mark.xfail
-@pytest.mark.parametrize('format',  ['xml', 'json-ld', 'ttl', 'n3'])
-def test_get_concept_rdf_error(application_settings, format, get_concept_rdf_url):
-    handler_concept = None
+@pytest.mark.parametrize('concept_uri',  ['http://vanhetneppadjeaf.com', 'file://bestaatnietman', 'geeneens een url', 'fake://hahahaha'])
+def test_get_concept_rdf__invalid_concept_uri(application_settings, concept_uri):
     try:
-        handler_concept = LODHandlerConcept(application_settings)
-        when(LODHandlerConcept).getConceptUri(DUMMY_SET, DUMMY_NOTATION).thenReturn(get_concept_rdf_url)
-        data, status_code, headers = handler_concept.getConceptRDF(DUMMY_SET, DUMMY_NOTATION, return_format=format)
-        assert status_code == 200
 
-    except ValueError as e:
-        assert APIUtil.valueErrorContainsErrorId(e, 'bad_request')
-        assert handler_concept is None
+        handler_concept = LODHandlerConcept(application_settings)
+        when(LODHandlerConcept).getConceptUri(DUMMY_SET, DUMMY_NOTATION).thenReturn(concept_uri)
+        data, status_code, headers = handler_concept.getConceptRDF(DUMMY_SET, DUMMY_NOTATION, return_format=format)
+        assert 'error' in data
+        assert APIUtil.matchesErrorId(data['error'], 'bad_request')
     finally:
         unstub()
 
+def test_get_concept_rdf__invalid_return_format(application_settings, get_concept_rdf_url):
+    try:
+
+        handler_concept = LODHandlerConcept(application_settings)
+        when(LODHandlerConcept).getConceptUri(DUMMY_SET, DUMMY_NOTATION).thenReturn(get_concept_rdf_url)
+        data, status_code, headers = handler_concept.getConceptRDF(DUMMY_SET, DUMMY_NOTATION, return_format='DIKKENEPZOOI')
+        assert 'error' in data
+        assert APIUtil.matchesErrorId(data['error'], 'bad_request')
+    finally:
+        unstub()
 
 @pytest.mark.parametrize('format',  ['xml', 'json-ld', 'ttl', 'n3'])
-def test_get_concept_rdf_succes(application_settings, format, get_concept_rdf_url):
+def test_get_concept_rdf__succes(application_settings, format, get_concept_rdf_url):
     try:
         handler_concept = LODHandlerConcept(application_settings)
         when(LODHandlerConcept).getConceptUri(DUMMY_SET, DUMMY_NOTATION).thenReturn(get_concept_rdf_url)
