7f132387ceacb5ac7ec61807b2a4a43c61aaebb1
wmelder <wmelder@beeldengeluid.nl>
2019-11-29 16:38:45 +0100

    replaced fixture for transform_data_test

--- src/tests/unit_tests/apis/lod/transform_data_test.py
+++ src/tests/unit_tests/apis/lod/transform_data_test.py
@@ -2,10 +2,8 @@ import xmltodict
 import json
 
 def test_xml_to_json(o_get_record):
-    # f = open(o_get_record, "r")
-    # xml_string = o_get_record
-    # json_string = json.dumps(xmltodict.parse(xml_string))
-    json_string = json.dumps(xmltodict.parse(o_get_record.read()))
+    xml_string = o_get_record.read()
+    json_string = json.dumps(xmltodict.parse(xml_string))
     print(json_string.replace("fe:", ""))
 
 
