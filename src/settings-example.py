d50e0ef1473d0995f35240dad0e0b4519cb54918
wmelder <wmelder@beeldengeluid.nl>
2021-05-04 17:59:39 +0200

    minor changes to conform to PEP8

--- src/settings-example.py
+++ src/settings-example.py
@@ -6,25 +6,22 @@ class Config(object):
     DEBUG = True
     CACHE_TYPE = 'SimpleCache'
 
-    # OAI_BASE_URL = 'http://dummy.oai.com'
-    # XSLT_FILE = os.path.abspath(os.path.join(base_path, 'resource', 'nisv-bg-oai2lod-v04.xsl'))
-
     STORAGE_BASE_URL = 'http://...'
 
     PROFILES = [
         {
-            'id' : 'http://data.rdlabs.beeldengeluid.nl/schema',
-            'schema' : '../resource/bengSchema.ttl',
-            'mapping' : '../resource/daan-mapping-storage.ttl',
-            'default' : True #this profile is loaded in memory by default
+            'id': 'http://data.rdlabs.beeldengeluid.nl/schema',
+            'schema': '../resource/bengSchema.ttl',
+            'mapping': '../resource/daan-mapping-storage.ttl',
+            'default': True  # this profile is loaded in memory by default
         },
         {
-            'id' : 'http://schema.org',
-            'schema' : '../resource/schema-dot-org.ttl',
-            'mapping' : '../resource/daan-mapping-schema-org.ttl'
+            'id': 'http://schema.org',
+            'schema': '../resource/schema-dot-org.ttl',
+            'mapping': '../resource/daan-mapping-schema-org.ttl'
         },
         {
-            'id' : 'oai',
-            'mapping' : '../resource/daan-mapping.ttl',
+            'id': 'oai',
+            'mapping': '../resource/daan-mapping.ttl',
         }
     ]
