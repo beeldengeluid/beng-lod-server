5e4759c813ca70ca100312607308d902c6ede591
Jaap Blom <jblom@beeldengeluid.nl>
2021-05-06 14:59:43 +0200

    updated settings

--- src/settings-example.py
+++ src/settings-example.py
@@ -1,3 +1,6 @@
+from apis.lod.DAANStorageLODHandler import DAANStorageLODHandler
+from apis.lod.SDOStorageLODHandler import SDOStorageLODHandler
+
 class Config(object):
     APP_HOST = '0.0.0.0'
     APP_PORT = 5309
@@ -5,8 +8,6 @@ class Config(object):
 
     DEBUG = True
     CACHE_TYPE = 'SimpleCache'
-
-    ONTOSPY_OUTPUT_DIR = 'static/ontospy'
 
     STORAGE_BASE_URL = 'http://...'
 
@@ -16,7 +17,9 @@ class Config(object):
             'uri' : 'http://data.rdlabs.beeldengeluid.nl/schema/',
             'prefix' : 'nisv', # based on @prefix nisv: <http://data.rdlabs.beeldengeluid.nl/schema/> .
             'schema' : '../resource/bengSchema.ttl',
-            'mapping' : '../resource/daan-mapping-storage.ttl'
+            'mapping' : '../resource/daan-mapping-storage.ttl',
+            'storage_handler' : DAANStorageLODHandler,
+            'default' : True #this profile is loaded in memory by default
         },
         {
             'title' : 'NISV Catalogue schema.org schema',
@@ -24,13 +27,13 @@ class Config(object):
             'prefix' : 'sdo', # based on @prefix sdo: <https://schema.org/> .
             'schema' : '../resource/schema-dot-org.ttl',
             'mapping' : '../resource/daan-mapping-schema-org.ttl',
-            'default' : True #this profile is loaded in memory by default
+            'storage_handler' : SDOStorageLODHandler
         },
         {
             'title' : 'NISV Catalogue OAI schema',
             'uri' : 'http://www.openarchives.org/OAI/2.0/oai_dc/', #temporary: taken from https://dltj.org/article/oai-pmh-namespaces/
             'prefix' : 'oai',
-            'schema' : None, # no ontospy dir is created if there is no schema
+            'schema' : None,
             'mapping' : '../resource/daan-mapping.ttl',
         }
     ]
