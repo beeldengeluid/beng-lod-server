2ddc37fc9b9eabd6542e1dfec3d9e1c30cc192f5
Willem Melder <wmelder@beeldengeluid.nl>
2021-06-29 10:50:56 +0200

    removed some commented code

--- src/apis/lod/SPARQLHandlerHDT.py
+++ src/apis/lod/SPARQLHandlerHDT.py
@@ -1,3 +1,4 @@
+import logging
 from util.APIUtil import APIUtil
 from rdflib import Graph
 from rdflib_hdt import HDTStore, optimize_sparql
@@ -15,33 +16,32 @@ class SPARQLHandlerHDT:
         self.hdt_file = self.config.get('HDT_FILE')
         assert self.hdt_file is not None, 'The configuration is missing an HDT file.'
         # self._init_hdt_doc()
-        # self._init_hdt_graph()
+        self._init_hdt_graph()
 
-    # def _init_hdt_doc(self):
-    #     """ Load an HDT file. Missing indexes are generated automatically, add False as the
-    #     second argument to disable them.
-    #     """
-    #
-    #     document = HDTDocument(self.hdt_file)
-    #
-    #     # Display some metadata about the HDT document itself
-    #     print(f"Number of RDF triples: {document.total_triples}")
-    #     print(f"Number of subjects: {document.nb_subjects}")
-    #     print(f"Number of predicates: {document.nb_predicates}")
-    #     print(f"Number of objects: {document.nb_objects}")
-    #     print(f"Number of shared subject-object: {document.nb_shared}")
-    #
-    # def _init_hdt_graph(self):
-    #     """ Load an HDT file. Missing indexes are generated automatically.
-    #     You can provide the index file by putting them in the same directory than the HDT file.
-    #     """
-    #     store = HDTStore(self.hdt_file)
-    #     # Display some metadata about the HDT document itself
-    #     print(f"Number of RDF triples: {len(store)}")
-    #     print(f"Number of subjects: {store.nb_subjects}")
-    #     print(f"Number of predicates: {store.nb_predicates}")
-    #     print(f"Number of objects: {store.nb_objects}")
-    #     print(f"Number of shared subject-object: {store.nb_shared}")
+    def _init_hdt_doc(self):
+        """ Load an HDT file. Missing indexes are generated automatically, add False as the
+        second argument to disable them.
+        """
+        document = HDTDocument(self.hdt_file)
+    
+        # Display some metadata about the HDT document itself
+        logging.debug(f"Number of RDF triples: {document.total_triples}")
+        logging.debug(f"Number of subjects: {document.nb_subjects}")
+        logging.debug(f"Number of predicates: {document.nb_predicates}")
+        logging.debug(f"Number of objects: {document.nb_objects}")
+        logging.debug(f"Number of shared subject-object: {document.nb_shared}")
+    
+    def _init_hdt_graph(self):
+        """ Load an HDT file. Missing indexes are generated automatically.
+        You can provide the index file by putting them in the same directory than the HDT file.
+        """
+        store = HDTStore(self.hdt_file)
+        # Display some metadata about the HDT document itself
+        logging.debug(f"Number of RDF triples: {len(store)}")
+        logging.debug(f"Number of subjects: {store.nb_subjects}")
+        logging.debug(f"Number of predicates: {store.nb_predicates}")
+        logging.debug(f"Number of objects: {store.nb_objects}")
+        logging.debug(f"Number of shared subject-object: {store.nb_shared}")
 
     def run_query(self, query=None):
         """ Runs a query against an HDT store."""
@@ -53,13 +53,6 @@ class SPARQLHandlerHDT:
         data_bytes = io.BytesIO()
         json_result_serializer.serialize(data_bytes, encoding='utf-8')
         # print(data_bytes.getvalue())
-        # TODO: find out how to use the parser
-        #json_result_parser = JSONResultParser(results)
-        #data_string = io.StringIO()
-        #json_results = json_result_parser.parse(data_string)
-        #for row in results:
-        #    print(row)
-        #    #print('%s %s %s' % row)
 
         return APIUtil.toSuccessResponse(data_bytes.getvalue())
 
