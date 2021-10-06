9eec8d0ed2a22dee44510660463a1727fde5b48a
wmelder <wmelder@beeldengeluid.nl>
2021-10-06 13:27:18 +0200

    initial commit for the license changes

--- src/models/SDORdfModel.py
+++ src/models/SDORdfModel.py
@@ -27,7 +27,9 @@ MENTIONS = SCHEMA_DOT_ORG_NAMESPACE + "mentions"
 ADDITIONAL_TYPE = SCHEMA_DOT_ORG_NAMESPACE + "additionalType"
 URL = SCHEMA_DOT_ORG_NAMESPACE + "url"
 CONDITIONS_OF_ACCESS = SCHEMA_DOT_ORG_NAMESPACE + "conditionsOfAccess"
-#IS_PART_OF_PROGRAM = SCHEMA_DOT_ORG_NAMESPACE + "isPartOfProgram"  # TODO work out how to handle this, is no equivalent property
+LICENSE = SCHEMA_DOT_ORG_NAMESPACE + "license"
+# TODO work out how to handle this, is no equivalent property
+#IS_PART_OF_PROGRAM = SCHEMA_DOT_ORG_NAMESPACE + "isPartOfProgram"
 
 # TODO: look at how to use the GTAA types
 # list of non-gtaa types, so we can use the correct namespace for concepts of this type
@@ -46,3 +48,27 @@ ROLE_TYPES = [PERSON, ORGANIZATION]
 CLASS_URIS_FOR_DAAN_LEVELS = {"SERIES": SERIES, "SEASON": SEASON, "PROGRAM": PROGRAM, "LOGTRACKITEM": CLIP,
                               "ITEM": CARRIER}
 
+# Creative Commons licenses
+CC_PDM = 'http://creativecommons.org/publicdomain/mark/1.0/'
+# CC0 = 'https://creativecommons.org/publicdomain/zero/1.0/'
+CC_BY = 'https://creativecommons.org/licenses/by/4.0/'
+CC_BY_SA = 'https://creativecommons.org/licenses/by-sa/4.0/'
+# CC_BY_NC = 'https://creativecommons.org/licenses/by-nc/4.0/'
+# CC_BY_NC_SA = 'https://creativecommons.org/licenses/by-nc-sa/4.0/'
+# CC_BY_ND = 'https://creativecommons.org/licenses/by-nd/4.0/'
+# CC_BY_NC_ND = 'https://creativecommons.org/licenses/by-nc-nd/4.0/'
+
+# rightsstatements.org licenses
+RS_IN_COPYRIGHT = 'http://rightsstatements.org/vocab/InC/1.0/'
+RS_EU_ORPHAN_WORK = 'http://rightsstatements.org/vocab/InC-OW-EU/1.0/'
+# RS_EDUCATIONAL_USE_PERMITTED = 'http://rightsstatements.org/vocab/InC-EDU/1.0/'
+# RS_CONTRACTUAL_RESTRICTIONS = 'http://rightsstatements.org/vocab/NoC-CR/1.0/'
+RS_COPYRIGHT_NOT_EVALUATED = 'http://rightsstatements.org/vocab/CNE/1.0/'
+
+# list of dutch public broadcast organizations
+DUTCH_PUBLIC_BROADCASTERS = ['HUMAN', 'HV', 'HOS', 'Humanistische Omroep', 'MAX', 'OMROEP MAX', 'VPRO', 'NOS',
+                             'NTS', 'NRU', 'NTR', 'NPS', 'Teleac', 'RVU', 'FEDUCO', 'FEDOCU', 'NOT', 'TELEAC',
+                             'TELEAC/NOT', 'STV', 'EO', 'IKOR', 'IKON', 'NIK MEDIA', 'Joodse Omroep', 'JOODSE OMROEP',
+                             'POWNED', 'AVRO/TROS', 'AVROTROS', 'AVRO', 'TROS', 'KRO-NCRV', 'KRO', 'NCRV', 'RKK',
+                             'BOS', 'BNN', 'VARA', 'BNNVARA', 'WNL'
+                             ]
