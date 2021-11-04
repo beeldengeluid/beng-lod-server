10340767d3c98fc45f84ded211d4d8b90360ebbe
wmelder <wmelder@beeldengeluid.nl>
2021-11-04 11:57:24 +0100

    bloody tests

--- src/config/settings_example.py
+++ src/config/settings_example.py
@@ -1,5 +1,5 @@
-from src.apis.resource.DAANStorageLODHandler import DAANStorageLODHandler
-from src.apis.resource.SDOStorageLODHandler import SDOStorageLODHandler
+from apis.resource.DAANStorageLODHandler import DAANStorageLODHandler
+from apis.resource.SDOStorageLODHandler import SDOStorageLODHandler
 
 
 class Config(object):
