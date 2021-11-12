e7bf5ba928d0511b0b22ca2093f465bb2cb0e9c0
wmelder <wmelder@beeldengeluid.nl>
2021-11-12 11:27:07 +0100

    small fix for reading the credentials from the config

--- src/apis/resource/resource_api.py
+++ src/apis/resource/resource_api.py
@@ -76,7 +76,7 @@ def prepare_lod_resource_uri(level, identifier):
     :param identifier: the DAAN id
     :returns: a proper URI as it should be listed in the LOD server.
     """
-    url_parts = urlparse(str(current_app.get('BENG_DATA_DOMAIN')))
+    url_parts = urlparse(str(current_app.config.get('BENG_DATA_DOMAIN')))
     if url_parts.netloc is not None:
         path = '/'.join(['id', level, str(identifier)])
         parts = (url_parts.scheme, url_parts.netloc, path, '', '', '')
@@ -100,8 +100,8 @@ class ResourceAPI(Resource):
         lod_url = prepare_lod_resource_uri(cat_type, identifier)
 
         # only registered user can access all items
-        auth_user = current_app.get('AUTH_USER')
-        auth_pass = current_app.get('AUTH_PASSWORD')
+        auth_user = current_app.config.get('AUTH_USER')
+        auth_pass = current_app.config.get('AUTH_PASSWORD')
         auth = request.authorization
         if auth is not None and auth.type == 'basic' and auth.username == auth_user and auth.password == auth_pass:
             # no restrictions, bypass the check
