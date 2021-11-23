893123e861b22cc3a76df3dd98dadcc2a0098c23
Jaap Blom <jblom@beeldengeluid.nl>
2021-11-23 11:11:05 +0100

    added more validation (existence of configured files & paths)

--- src/tests/base_test.py
+++ src/tests/base_test.py
@@ -8,5 +8,6 @@ Tests everything related to the validity of the application settings (settings.p
 def test_settings_exist(base_file_path):
     assert os.path.exists(os.path.join(base_file_path, 'config', 'settings_example.py'))
 
+# validate the settings_example.py, but do not validate the file paths, since they do not have to exist
 def test_settings_valid(application_settings):
-    assert validate_config(application_settings)
+    assert validate_config(application_settings, False)
