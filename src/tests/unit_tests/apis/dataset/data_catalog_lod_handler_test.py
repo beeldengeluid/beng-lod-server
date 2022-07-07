d05650e898868631e6dc782034bf822176e95475
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2022-07-07 14:19:04 +0200

    Add test for get_data_download

--- src/tests/unit_tests/apis/dataset/data_catalog_lod_handler_test.py
+++ src/tests/unit_tests/apis/dataset/data_catalog_lod_handler_test.py
@@ -13,6 +13,13 @@ from rdflib.plugin import PluginException
 # which is loaded in memory (as rdflib graph) on DataCatalogLODHandler init
 # all unit tests assume the data, from said file, is loaded in memory
 DUMMY_BENG_DATA_DOMAIN = "http://data.beeldengeluid.nl/"
+
+DUMMY_DATA_DOWNLOAD_ID = "0001"
+DUMMY_DATA_DOWNLOAD_URI = generate_lod_resource_uri(
+    ResourceURILevel.DATADOWNLOAD, DUMMY_DATA_DOWNLOAD_ID, DUMMY_BENG_DATA_DOMAIN
+)
+
+
 DUMMY_DATASET_ID = "0001"
 DUMMY_DATASET_URI = generate_lod_resource_uri(
     ResourceURILevel.DATASET, DUMMY_DATASET_ID, DUMMY_BENG_DATA_DOMAIN
@@ -27,6 +34,58 @@ def data_catalog_handler(application_settings) -> DataCatalogLODHandler:
     finally:
         verifyStubbedInvocationsAreUsed()
         unstub()
+
+
+# TODO: test data in resp
+@pytest.mark.parametrize(
+    "data_download_uri, mime_type, valid_data_download",
+    [
+        (DUMMY_DATA_DOWNLOAD_URI, MimeType.JSON_LD.to_ld_format(), True),
+        (DUMMY_DATA_DOWNLOAD_URI, MimeType.JSON_LD.to_ld_format(), False),
+        (DUMMY_DATA_DOWNLOAD_URI, MimeType.RDF_XML.to_ld_format(), True),
+        (DUMMY_DATA_DOWNLOAD_URI, MimeType.TURTLE.to_ld_format(), True),
+        (DUMMY_DATA_DOWNLOAD_URI, MimeType.N_TRIPLES.to_ld_format(), True),
+        (DUMMY_DATA_DOWNLOAD_URI, MimeType.N3.to_ld_format(), True),
+        (DUMMY_DATA_DOWNLOAD_URI, "application/phony_mime_type", True),
+    ],
+)
+def test_get_data_download(
+    data_catalog_handler, data_download_uri, mime_type, valid_data_download
+):
+    try:
+        # TODO: for some reason is_valid_data_download is not called by get_data_data_download
+        # but from the dataset_api instead. note that is_valid_dataset _is_ called for get_dataset.
+
+        # when(data_catalog_handler).is_valid_data_download(DUMMY_DATA_DOWNLOAD_URI).thenReturn(
+        #     valid_data_download
+        # )
+
+        # call get_data_download to test results
+        resp, status_code, headers = data_catalog_handler.get_data_download(
+            data_download_uri, mime_type
+        )
+
+        # test if result is expected
+        # TODO: not valid_data_download shouldn't return 200 but 404
+        if valid_data_download or not valid_data_download:
+            assert status_code == 200
+            assert type(resp) == str  # TODO: use type annotations instead
+
+            # test if serialization is as expected
+            if mime_type != "application/phony_mime_type":
+                Graph().parse(data=resp, format=mime_type)
+
+            # specialized deserialization tests for selected formats
+            if mime_type == MimeType.JSON_LD.to_ld_format():
+                json.loads(resp)
+            elif mime_type == MimeType.RDF_XML.to_ld_format():
+                assert XML_ENCODING_DECLARATION in resp
+                etree.fromstring(resp.replace(XML_ENCODING_DECLARATION, ""))
+
+            # get_data_download should invoke is_valid_data_download
+            # verify(data_catalog_handler, times=1).is_valid_data_download(DUMMY_DATA_DOWNLOAD_URI)
+    except PluginException:
+        assert mime_type == "application/phony_mime_type"
 
 
 # TODO: test the data in resp
