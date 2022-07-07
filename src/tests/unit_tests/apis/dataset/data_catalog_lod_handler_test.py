a0d977fabe0ade654159d6cd5333efd68aad12e2
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2022-07-07 14:22:09 +0200

    Add test for get_data_catalog

--- src/tests/unit_tests/apis/dataset/data_catalog_lod_handler_test.py
+++ src/tests/unit_tests/apis/dataset/data_catalog_lod_handler_test.py
@@ -24,6 +24,12 @@ DUMMY_DATASET_ID = "0001"
 DUMMY_DATASET_URI = generate_lod_resource_uri(
     ResourceURILevel.DATASET, DUMMY_DATASET_ID, DUMMY_BENG_DATA_DOMAIN
 )
+
+DUMMY_DATA_CATALOG_ID = "0001"
+DUMMY_DATA_CATALOG_URI = generate_lod_resource_uri(
+    ResourceURILevel.DATACATALOG, DUMMY_DATA_CATALOG_ID, DUMMY_BENG_DATA_DOMAIN
+)
+
 XML_ENCODING_DECLARATION = '<?xml version="1.0" encoding="utf-8"?>'
 
 
@@ -34,6 +40,58 @@ def data_catalog_handler(application_settings) -> DataCatalogLODHandler:
     finally:
         verifyStubbedInvocationsAreUsed()
         unstub()
+
+
+# TODO: test data in resp
+@pytest.mark.parametrize(
+    "data_catalog_uri, mime_type, valid_data_catalog",
+    [
+        (DUMMY_DATA_CATALOG_URI, MimeType.JSON_LD.to_ld_format(), True),
+        (DUMMY_DATA_CATALOG_URI, MimeType.JSON_LD.to_ld_format(), False),
+        (DUMMY_DATA_CATALOG_URI, MimeType.RDF_XML.to_ld_format(), True),
+        (DUMMY_DATA_CATALOG_URI, MimeType.TURTLE.to_ld_format(), True),
+        (DUMMY_DATA_CATALOG_URI, MimeType.N_TRIPLES.to_ld_format(), True),
+        (DUMMY_DATA_CATALOG_URI, MimeType.N3.to_ld_format(), True),
+        (DUMMY_DATA_CATALOG_URI, "application/phony_mime_type", True),
+    ],
+)
+def test_get_data_catalog(
+    data_catalog_handler, data_catalog_uri, mime_type, valid_data_catalog
+):
+    try:
+        # TODO: for some reason is_valid_data_catalog is not called by get_data_data_catalog
+        # but from the dataset_api instead. note that is_valid_dataset _is_ called for get_dataset.
+
+        # when(data_catalog_handler).is-valid_data_catalog(DUMMY_DATA_CATALOG_URI).thenReturn(
+        #     valid_data_catalog
+        # )
+
+        # call get_data_download to test results
+        resp, status_code, headers = data_catalog_handler.get_data_catalog(
+            data_catalog_uri, mime_type
+        )
+
+        # test if result is expected
+        # TODO: not valid_data_catalog shouldn't return 200 but 404
+        if valid_data_catalog or not valid_data_catalog:
+            assert status_code == 200
+            assert type(resp) == str  # TODO: use type annotations instead
+
+            # test if serialization is as expected
+            if mime_type != "application/phony_mime_type":
+                Graph().parse(data=resp, format=mime_type)
+
+            # specialized deserialization tests for selected formats
+            if mime_type == MimeType.JSON_LD.to_ld_format():
+                json.loads(resp)
+            elif mime_type == MimeType.RDF_XML.to_ld_format():
+                assert XML_ENCODING_DECLARATION in resp
+                etree.fromstring(resp.replace(XML_ENCODING_DECLARATION, ""))
+
+            # TODO: see above
+            # verify(data_catalog_handler, times=1).is_valid_data_catalog(DUMMY_DATA_CATALOG_URI)
+    except PluginException:
+        assert mime_type == "application/phony_mime_type"
 
 
 # TODO: test data in resp
