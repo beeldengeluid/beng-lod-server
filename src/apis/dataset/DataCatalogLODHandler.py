ad3a721f2183c7735fef7d31ec589402ff767b05
wmelder <wmelder@beeldengeluid.nl>
2022-07-12 11:06:47 +0200

    separation of API and RDF in DataCatalogHandler

--- src/apis/dataset/DataCatalogLODHandler.py
+++ src/apis/dataset/DataCatalogLODHandler.py
@@ -1,9 +1,9 @@
 import logging
-from util.APIUtil import APIUtil
 from rdflib import URIRef
 from rdflib import Graph
 from rdflib.namespace import RDF, SDO
 from apis.mime_type_util import MimeType
+from typing import List, Optional
 
 
 class DataCatalogLODHandler:
@@ -56,7 +56,7 @@ class DataCatalogLODHandler:
             return True
         return False
 
-    def is_valid_dataset(self, dataset_id):
+    def is_valid_dataset(self, dataset_id: str):
         """Checks whether the dataset qualifies according to the NDE requirements for datasets.
 
         A Dataset MUST have:
@@ -87,7 +87,7 @@ class DataCatalogLODHandler:
             return True
         return False
 
-    def is_valid_data_catalog(self, data_catalog_id):
+    def is_valid_data_catalog(self, data_catalog_id: str) -> bool:
         """Checks if the data catalog has at least got the minimal required properties.
 
         The DataCatalog has at least:
@@ -132,194 +132,183 @@ class DataCatalogLODHandler:
 
     """------------LOD Handler functions---------------"""
 
-    def get_data_download(self, data_download_id, mime_format=MimeType.JSON_LD.value):
+    def is_data_download(self, data_download_id: str) -> bool:
+        """Check if data download exists."""
+        return (
+            URIRef(data_download_id),
+            RDF.type,
+            SDO.DataDownload,
+        ) in self._data_catalog
+
+    def get_data_download(
+        self, data_download_id: str, mime_format: str = "json-ld"
+    ) -> str:
         """Get the triples for a DataDownload.
         :param data_download_id: the id of a DataDownload, originating from the spreadsheet. Must be a URI.
-        :param mime_format: mime_type for the data response.
-        :returns: The triples (or the Graph?)  for the DataDownload.
+        :param mime_format: the required mime_type to return
+        :returns: The Graph for the DataDownload.
         """
-        if not (URIRef(data_download_id), None, None) in self._data_catalog:
-            return APIUtil.toErrorResponse("not_found")
-
         g = Graph()
         g.bind("sdo", SDO)
-
-        # load the triples for the data download
-        for triple in self.triples_data_download(data_download_uri=data_download_id):
+        for triple in self._data_catalog.triples(
+            (URIRef(data_download_id), None, None)
+        ):
             g.add(triple)
 
         # now return the collected triples in the requested format
-        json_string = g.serialize(
+        res_string = g.serialize(
             format=mime_format,
             auto_compact=True,
         )
-        if json_string:
-            return APIUtil.toSuccessResponse(json_string)
-        return APIUtil.toErrorResponse("bad_request", "Invalid URI or return format")
+        return res_string
+
+    def is_data_catalog(self, data_catalog_uri: str) -> bool:
+        """Check if data catalog exists"""
+        return (
+            URIRef(data_catalog_uri),
+            RDF.type,
+            SDO.DataCatalog,
+        ) in self._data_catalog
 
     def get_data_catalog(self, data_catalog_uri, mime_format=MimeType.JSON_LD.value):
         """Returns the data from the data catalog graph in requested serialization format.
         :param data_catalog_uri: the identifier for the data catalog
         :param mime_format: the requested mime type for the graph data. Defaults to JSON-LD.
         """
-        # check if resource exists
-        if not (URIRef(data_catalog_uri), None, None) in self._data_catalog:
-            return APIUtil.toErrorResponse("not_found")
-
         g = Graph()
         g.bind("sdo", SDO)
 
         # add triples for the data_catalog
-        for triple in self.triples_data_catalog(data_catalog_uri=data_catalog_uri):
-            g.add(triple)
+        g += self.get_data_catalog_triples(URIRef(data_catalog_uri))
 
-        # add triples for the organization
-        organization_id = self.get_organization_for_data_catalog(
-            data_catalog_id=data_catalog_uri
-        )
-        for triple in self.triples_organization(organization_id=organization_id):
-            g.add(triple)
+        # add triples for the publisher
+        publisher_id = self.get_publisher_for_data_catalog(URIRef(data_catalog_uri))
+        g += self.get_organization_triples(publisher_id)
 
         # add triples for each dataset
-        for dataset_uri in self.get_datasets_for_data_catalog(
-            data_catalog_id=data_catalog_uri
-        ):
-            for triple in self.triples_dataset(dataset_uri=dataset_uri):
-                g.add(triple)
+        for dataset_uri in self.get_datasets_for_data_catalog(URIRef(data_catalog_uri)):
+            g += self.get_dataset_triples(URIRef(dataset_uri))
 
-            # add the triples for the organization
-            organization_id = self.get_organization_for_dataset(dataset_id=dataset_uri)
-            for triple in self.triples_organization(organization_id=organization_id):
-                g.add(triple)
-
-            # add triples for each data download
-            for data_download_id in self.get_data_downloads_for_dataset(
-                dataset_id=dataset_uri
-            ):
-                for triple in self.triples_data_download(
-                    data_download_uri=data_download_id
-                ):
-                    g.add(triple)
-
-        # now return the collected triples in the requested format
-        json_string = g.serialize(
+        # now serialize the collected triples in the requested format
+        res_string = g.serialize(
             format=mime_format,
             auto_compact=True,
         )
+        return res_string
 
-        if json_string:
-            return APIUtil.toSuccessResponse(json_string)
-        return APIUtil.toErrorResponse("bad_request", "Invalid URI or return format")
+    def is_dataset(self, dataset_uri: str) -> bool:
+        """Check if dataset exists"""
+        return (
+            URIRef(dataset_uri),
+            RDF.type,
+            SDO.Dataset,
+        ) in self._data_catalog
 
-    def get_dataset(self, dataset_uri, mime_format="json-ld"):
+    def get_dataset(self, dataset_uri: str, mime_format: str = "json-ld") -> str:
         """Returns the data from the data catalog graph in requested serialization format.
         :param dataset_uri: the identifier for the dataset
         :param mime_format: the requested mime type for the graph data. Defaults to JSON-LD.
         """
-        # check if resource exists
-        if not (URIRef(dataset_uri), None, None) in self._data_catalog:
-            return APIUtil.toErrorResponse("not_found")
-
         g = Graph()
         g.bind("sdo", SDO)
 
-        if self.is_valid_dataset(dataset_uri):
+        # add triples for the dataset
+        g += self.get_dataset_triples(URIRef(dataset_uri))
 
-            # add triples for the dataset
-            for triple in self.triples_dataset(dataset_uri=dataset_uri):
-                g.add(triple)
-
-            # get triples for all the data downloads in the dataset
-            for data_download_id in self.get_data_downloads_for_dataset(dataset_uri):
-                for triple in self.triples_data_download(
-                    data_download_uri=data_download_id
-                ):
-                    g.add(triple)
-
-            # add the triples for the organization
-            organization_id = self.get_organization_for_dataset(dataset_id=dataset_uri)
-            for triple in self.triples_organization(organization_id=organization_id):
-                g.add(triple)
-
-            # FIX #240 if the dataset has a creator, add the creator organization as well
-            creator_id = self.get_creator_for_dataset(dataset_id=dataset_uri)
-            for triple in self.triples_organization(organization_id=creator_id):
-                g.add(triple)
-
-        json_string = g.serialize(
+        res_string = g.serialize(
             format=mime_format,
             auto_compact=True,
         )
-        if json_string:
-            return APIUtil.toSuccessResponse(json_string)
-        return APIUtil.toErrorResponse("bad_request", "Invalid URI or return format")
+        return res_string
 
     """------------Triples filters---------------"""
 
-    def triples_data_download(self, data_download_uri):
-        """Returns a generator over the triples for the data download."""
-        if data_download_uri is None:
-            return None
-        data_download_triples = self._data_catalog.triples(
-            (URIRef(data_download_uri), None, None)
-        )
-        return data_download_triples if data_download_triples is not None else None
+    def get_data_download_triples(self, data_download_uri: URIRef) -> Graph:
+        """Adds the triples for the data download to a Graph."""
+        g = Graph()
+        g.bind("sdo", SDO)
+        for triple in self._data_catalog.triples((data_download_uri, None, None)):
+            g.add(triple)
+        return g
 
-    def triples_dataset(self, dataset_uri):
-        """Returns a generator over the triples for the dataset. It includes the triples for the dataset."""
-        if dataset_uri is None:
-            return None
-        dataset_triples = self._data_catalog.triples((URIRef(dataset_uri), None, None))
-        return dataset_triples if dataset_triples is not None else None
+    def get_dataset_triples(self, dataset_uri: URIRef) -> Graph:
+        """Returns the graph containing all triples for a dataset."""
+        g = Graph()
+        g.bind("sdo", SDO)
 
-    def triples_data_catalog(self, data_catalog_uri):
-        """Returns a generator over the triples for the data catalog."""
-        if data_catalog_uri is None:
-            return None
-        triples_data_catalog = self._data_catalog.triples(
-            (URIRef(data_catalog_uri), None, None)
-        )
-        return triples_data_catalog if triples_data_catalog is not None else None
+        # add triples related to the dataset itself
+        for triple in self._data_catalog.triples((dataset_uri, None, None)):
+            g.add(triple)
 
-    def triples_organization(self, organization_id):
-        """Returns a generator over the triples for the organization."""
-        if organization_id is None:
-            return None
-        organization_triples = self._data_catalog.triples(
-            (URIRef(organization_id), None, None)
-        )
-        return organization_triples if organization_triples is not None else None
+        # add the triples for the publisher
+        publisher_id = self.get_publisher_for_dataset(dataset_uri)
+        g += self.get_organization_triples(publisher_id)
 
-    """-------------parts of functions------------------"""
+        # if the dataset has a creator, add the triples for the creator organization
+        creator_id = self.get_creator_for_dataset(dataset_uri)
+        if creator_id is not None:
+            g += self.get_organization_triples(creator_id)
 
-    def get_datasets_for_data_catalog(self, data_catalog_id):
+        # get triples for all the data downloads in the dataset
+        for data_download_id in self.get_data_downloads_for_dataset(dataset_uri):
+            g += self.get_data_download_triples(data_download_id)
+
+        return g
+
+    def get_data_catalog_triples(self, data_catalog_uri: URIRef) -> Graph:
+        """Adds the triples for a datasets to a Graph and returns the Graph."""
+        g = Graph()
+        g.bind("sdo", SDO)
+        for triple in self._data_catalog.triples((data_catalog_uri, None, None)):
+            g.add(triple)
+        return g
+
+    """------------- get aggregates ------------------"""
+
+    def get_datasets_for_data_catalog(self, data_catalog_id: str) -> List:
         """Return a list of dataset_id's that are a dataset for the data datalog."""
         list_of_datasets = []
         for dataset_obj in self._data_catalog.objects(
             URIRef(data_catalog_id), SDO.dataset
         ):
-            if self.is_valid_dataset(dataset_id=dataset_obj):
+            if self.is_valid_dataset(dataset_obj):
                 list_of_datasets.append(dataset_obj)
         return list_of_datasets
 
-    def get_data_downloads_for_dataset(self, dataset_id):
+    def get_data_downloads_for_dataset(self, dataset_id: str) -> List[URIRef]:
         """Return a list of data_downloads_id's that are a distribution of the dataset."""
         list_of_distributions = []
         for data_download_obj in self._data_catalog.objects(
             URIRef(dataset_id), SDO.distribution
         ):
-            if self.is_valid_data_download(data_download_id=data_download_obj):
+            if self.is_valid_data_download(data_download_obj):
                 list_of_distributions.append(data_download_obj)
         return list_of_distributions
 
-    def get_organization_for_data_catalog(self, data_catalog_id):
+    def get_publisher_for_data_catalog(
+        self, data_catalog_id: URIRef
+    ) -> Optional[URIRef]:
         """Return the URI for the organization that is the publisher of the DataCatalog."""
         return self._data_catalog.value(URIRef(data_catalog_id), SDO.publisher)
 
-    def get_organization_for_dataset(self, dataset_id):
-        """Return the URI for the organization that is the publisher of the Dataset."""
-        return self._data_catalog.value(URIRef(dataset_id), SDO.publisher)
+    def is_organization(self, organization_id: URIRef) -> bool:
+        """Check whether organization exists in data catalog."""
+        return (organization_id, RDF.type, SDO.Organization) in self._data_catalog
 
-    def get_creator_for_dataset(self, dataset_id):
+    def get_organization_triples(self, organization_id: URIRef) -> Graph:
+        """Returns a Graph with the triples for the organization."""
+        g = Graph()
+        g.bind("sdo", SDO)
+        if organization_id is None or self.is_organization(organization_id) is False:
+            return g
+        for triple in self._data_catalog.triples((organization_id, None, None)):
+            g.add(triple)
+        return g
+
+    def get_publisher_for_dataset(self, dataset_id: URIRef) -> URIRef:
+        """Return the URI for the organization that is the publisher of the Dataset."""
+        return self._data_catalog.value(dataset_id, SDO.publisher)
+
+    def get_creator_for_dataset(self, dataset_id: URIRef) -> URIRef:
         """Return the URI for the Organization that is the creator of the Dataset."""
-        return self._data_catalog.value(URIRef(dataset_id), SDO.creator)
+        return self._data_catalog.value(dataset_id, SDO.creator)
