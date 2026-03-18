ce4f3b081d9b12414d5a7e26f2fdbbd7884e25d0
Willem Melder <wmelder@beeldengeluid.nl>
2026-03-18 15:26:28 +0100

    merged mw_lodview and lodview tests

--- src/tests/unit_tests/util/lodview_util_test.py
+++ src/tests/unit_tests/util/lodview_util_test.py
@@ -1,14 +1,8 @@
-from rdflib import URIRef, Literal, BNode
-from rdflib.namespace import RDF, SDO  # type: ignore
+import pytest
+from rdflib import Graph, URIRef, Literal, BNode
+from rdflib.namespace import RDF, RDFS, SKOS, SDO, DCTERMS  # type: ignore
 from mockito import unstub
-from util.lodview_util import (
-    json_header_from_rdf_graph,
-    json_iri_iri_from_rdf_graph,
-    json_iri_lit_from_rdf_graph,
-    json_iri_bnode_from_rdf_graph,
-    get_lod_view_resource_header,
-    get_lod_view_resource,
-)
+import util.lodview_util
 
 DUMMY_BENG_DATA_DOMAIN = "http://data.beeldengeluid.nl/"  # see setting_example.py
 DUMMY_RESOURCE_ID = "1234"
@@ -17,7 +11,9 @@ DUMMY_RESOURCE_URI = f"{DUMMY_BENG_DATA_DOMAIN}id/scene/{DUMMY_RESOURCE_ID}"  # 
 
 def test_json_header_from_rdf_graph(scene_rdf_graph):
     try:
-        ui_data = json_header_from_rdf_graph(scene_rdf_graph, DUMMY_RESOURCE_URI)
+        ui_data = util.lodview_util.json_header_from_rdf_graph(
+            scene_rdf_graph, DUMMY_RESOURCE_URI
+        )
         assert isinstance(ui_data, list)
         assert len(ui_data) == 1
         for ui_item in ui_data:
@@ -38,7 +34,9 @@ def test_json_header_from_rdf_graph(scene_rdf_graph):
 
 def test_json_iri_iri_from_rdf_graph(scene_rdf_graph):
     try:
-        ui_data = json_iri_iri_from_rdf_graph(scene_rdf_graph, DUMMY_RESOURCE_URI)
+        ui_data = util.lodview_util.json_iri_iri_from_rdf_graph(
+            scene_rdf_graph, DUMMY_RESOURCE_URI
+        )
         assert isinstance(ui_data, list)
         assert len(ui_data) == 4
         for item in ui_data:
@@ -65,7 +63,9 @@ def test_json_iri_iri_from_rdf_graph(scene_rdf_graph):
 
 def test_json_iri_lit_from_rdf_graph(scene_rdf_graph):
     try:
-        ui_data = json_iri_lit_from_rdf_graph(scene_rdf_graph, DUMMY_RESOURCE_URI)
+        ui_data = util.lodview_util.json_iri_lit_from_rdf_graph(
+            scene_rdf_graph, DUMMY_RESOURCE_URI
+        )
         assert isinstance(ui_data, list)
         assert len(ui_data) == 1
         for item in ui_data:
@@ -104,7 +104,7 @@ def test_json_iri_bnode_from_rdf_graph(program_rdf_graph_with_bnodes):
             )
             if p != RDF.type and isinstance(o, BNode)
         )
-        ui_data = json_iri_bnode_from_rdf_graph(
+        ui_data = util.lodview_util.json_iri_bnode_from_rdf_graph(
             program_rdf_graph_with_bnodes, resource_iri
         )
         assert isinstance(ui_data, list)
@@ -162,7 +162,9 @@ def test_lod_view_resource_header(flask_test_client):
                 },
             }
         ]
-        header = get_lod_view_resource_header(mock_header, "http://example.com")
+        header = util.lodview_util.get_lod_view_resource_header(
+            mock_header, "http://example.com"
+        )
         print(header)
 
 
@@ -182,7 +184,7 @@ def test_get_lod_view_resource(
         title_node = program_rdf_graph.value(
             subject=resource_iri_node, predicate=SDO.name
         )
-        html_content = get_lod_view_resource(
+        html_content = util.lodview_util.get_lod_view_resource(
             program_rdf_graph,
             resource_iri,
             application_settings.get("SPARQL_ENDPOINT", ""),
@@ -198,3 +200,82 @@ def test_get_lod_view_resource(
         assert f"<h1>{title}</h1>" in html_content  # title should be in h1 tag
         resource_id_in_header = f"""<a class="link-light" title="&lt;{resource_iri}&gt;" href="{resource_iri}" target="_blank">&lt;{resource_iri}&gt;</a>"""
         assert resource_id_in_header in html_content  # resource IRI should be in header
+
+
+@pytest.mark.parametrize(
+    "iri,expected",
+    [
+        (
+            "http://example.org/property",
+            {
+                "uri": "http://example.org/property",
+                "prefix": "ex",
+                "namespace": "http://example.org/",
+                "property": "property",
+            },
+        ),
+        (
+            "http://example.org/",
+            {
+                "uri": "http://example.org/",
+                "prefix": "",
+                "namespace": "",
+                "property": "",
+            },
+        ),
+    ],
+)
+def test_json_parts_from_IRI(iri: str, expected: dict):
+    """Deconstruct the given IRI into its parts and return them in a dict.
+    The parts are prefix, namespace and property."""
+    DUMMY_GRAPH = Graph()
+    DUMMY_GRAPH.bind("ex", "http://example.org/")
+    result = util.lodview_util.json_parts_from_IRI(DUMMY_GRAPH, iri)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "label_type", [RDFS.label, SKOS.prefLabel, SDO.name, DCTERMS.title]
+)
+@pytest.mark.parametrize("lang", ["", "nl", "en"])
+def test_json_label_for_node(label_type: URIRef, lang: str):
+    """Generates a list of all possible labels for a given IRI or BNode.
+    This list is used for the visualisation of the resource in the LOD view.
+    :param lang: the language preference for the labels.
+    """
+    DUMMY_URI = URIRef("http://example.org/01")
+    DUMMY_LITERAL = Literal("test", lang="nl")
+    test_graph = Graph(bind_namespaces="core")
+    test_graph.add((DUMMY_URI, label_type, DUMMY_LITERAL))
+    result = util.lodview_util.json_label_for_node(test_graph, DUMMY_URI, lang)
+    assert isinstance(result, list), "Apparently the wrong type is returned."
+    for res in result:
+        assert isinstance(res, str), "Apparently the wrong type is returned."
+    if lang == "nl":
+        assert result == ["test"], "Apparently the wrong label is returned."
+    elif lang == "en":
+        assert result == ["test @nl"], "Apparently the wrong label is returned."
+    elif lang == "":
+        assert result == ["test @nl"], "Apparently the wrong label is returned."
+    else:
+        assert result == ["test @nl"], "Apparently the wrong label is returned."
+
+
+@pytest.mark.parametrize(
+    "literal,lang,expected",
+    [
+        (Literal("test", lang="nl"), "nl", "test"),
+        (Literal("test", lang="nl"), "en", "test @nl"),
+        (Literal("test", lang="nl"), "", "test @nl"),
+        (Literal("test"), "nl", "test"),
+        (Literal("test"), "en", "test"),
+        (Literal("test"), "", "test"),
+    ],
+)
+def test_get_string_for_langstring(literal: Literal, lang: str, expected: str):
+    """Tests the get_string_for_langstring function. Given a literal with or without
+     a language tag, the function should return the string value of a literal, taking
+    into account the language.
+    """
+    result = util.lodview_util.get_string_for_langstring(literal, lang)
+    assert result == expected
